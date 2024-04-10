15350c97c56e92b167e71a603ba15844ea47a4ea
yogico1 <120371874+yogico1@users.noreply.github.com>
2024-04-10 13:10:03 +0900

    Add files via upload

--- server.js
+++ server.js
@@ -1,13 +1,10 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-
+// 정적 파일 제공
 app.use(express.static(path.join(__dirname, 'yogico/build')));
 
-app.get('/', function (요청, 응답) {
-  응답.sendFile(path.join(__dirname, '/yogico/build/index.html'));
-});
-
+// 모든 경로에 대해 index.html로 리다이렉션
 app.get('*', function (요청, 응답) {
   응답.sendFile(path.join(__dirname, '/yogico/build/index.html'));
 });
