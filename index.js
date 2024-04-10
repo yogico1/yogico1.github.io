6719cd68ac77d768191475269cfd4fdd6f02a09b
yogico1 <120371874+yogico1@users.noreply.github.com>
2024-04-10 13:09:07 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -3,9 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
-import { useLocation } from 'react-router-dom'; // useLocation 불러오기
-
+import { BrowserRouter ,useLocation} from 'react-router-dom';
 import { Provider } from "react-redux";
 import store from './store.js'
 
