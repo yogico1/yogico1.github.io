ed71a59bb7722ccc2b813469b7aae4e8166df5a3
yogico1 <120371874+yogico1@users.noreply.github.com>
2024-08-20 16:45:15 +0900

    Add files via upload

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-// 정적 파일 제공
-app.use(express.static(path.join(__dirname, 'yogico/build')));
 
-// 모든 경로에 대해 index.html로 리다이렉션
-app.get('*', function (요청, 응답) {
-  응답.sendFile(path.join(__dirname, '/yogico/build/index.html'));
+// 정적 파일 제공
+app.use(express.static(path.join(__dirname, 'build')));
+
+// 모든 경로를 index.html로 리디렉션하여 React Router가 처리하게 함
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+// 서버 시작
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
